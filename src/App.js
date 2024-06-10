8fca4c289f5bf9b56bd14f1cf2fcd20a58c47397
ZHARFAN <zharfanwidyadana@gmail.com>
2024-06-10 19:23:36 +0700

    tambah "h"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h>Klan Uchiha merupakan klan yang sangat spesial, Salah satunya yaitu matanya.</h>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
